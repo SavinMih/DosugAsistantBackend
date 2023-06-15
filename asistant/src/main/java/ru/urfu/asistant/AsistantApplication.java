8c85ac22d9b31e516c4b3585821ac6db1e85ad0d
MEMass <sawin.world@gmail.com>
2023-06-15 14:56:04 +0500

    first commit

---
+++ asistant/src/main/java/ru/urfu/asistant/AsistantApplication.java
@@ -0,0 +1,13 @@
+package ru.urfu.asistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsistantApplication.class, args);
+	}
+
+}
---
+++ asistant/src/test/java/ru/urfu/asistant/AsistantApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.urfu.asistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
